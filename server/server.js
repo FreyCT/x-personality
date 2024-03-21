ce57c8898868d7039c11ad6c21bef9cc272d528f
Freyden Tolman <Freydenit@gmail.com>
2024-03-21 15:15:58 -0700

    finished get and post for images/results

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const cors = require("cors");
 const express = require("express");
-const questionsRouter = require("./questionsRoute/questionsRoute");
+const fs = require("fs");
+
 const app = express();
 const PORT = 8088;
 
@@ -9,13 +10,26 @@ app.use(express.json());
 
 app.use(express.static("images"));
 
+const readFile = path => JSON.parse(fs.readFileSync(path));
 
-app.get("/", (req, res) => {
-  res.send("It's working!");
+app.get("/questions", (req, res) => {
+    const question = readFile("./data/questionnaire.json");
+    res.json(question);
 });
 
-app.use("/questions", questionsRouter);
+app.post("/questions", (req, res) => {
+
+    const resultsData = readFile("./data/resultsData.json");
+    const type = resultsData.mbti.find(t => t.type === req.body.answer);
+    res.json(type);
+});
+
+app.get("/images", (req, res) => {
+    const data = readFile("./data/resultsData.json");
+    const images = data.mbti.map(t => t.imageURL);
+    res.json(images);
+});
 
 app.listen(PORT, () => {
-  console.log(`Listening to port ${PORT}.`);
+    console.log(`Listening to port ${PORT}.`);
 });
